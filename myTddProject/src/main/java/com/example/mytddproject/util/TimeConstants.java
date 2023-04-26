cd688bf01b1db0ac11a098f18ddc2341e1c50a48
Victor Shulpov (NB) <vshulpov@list.ru>
2023-04-26 15:56:30 +0700

    added default class with time constants

---
+++ myTddProject/src/main/java/com/example/mytddproject/util/TimeConstants.java
@@ -0,0 +1,10 @@
+package com.example.mytddproject.util;
+
+public class TimeConstants {
+    public static final long SECOND = 1000;
+    public static final long MINUTE = SECOND * 60;
+    public static final long HOUR = MINUTE * 60;
+    public static final long DAY = HOUR * 24;
+    public static final long MONTH = DAY * 30;
+    public static final long YEAR = DAY * 365;
+}
