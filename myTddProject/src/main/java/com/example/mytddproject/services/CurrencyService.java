64fcc1755f1c788bfac4ffff6822e21a12673553
Victor Shulpov (NB) <vshulpov@list.ru>
2023-04-26 16:41:27 +0700

    added constants in CurrencyService

--- myTddProject/src/main/java/com/example/mytddproject/services/CurrencyService.java
+++ myTddProject/src/main/java/com/example/mytddproject/services/CurrencyService.java
@@ -3,12 +3,22 @@ package com.example.mytddproject.services;
 import com.example.mytddproject.dto.DayCurrencyDto;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 import static com.example.mytddproject.util.Utils.getStringFormatDate;
 
 public class CurrencyService {
+
+    public List<List<String>> currencyCodes = new ArrayList<>();
+    {
+        currencyCodes.add(Arrays.asList("Доллар США", "R01235"));
+        currencyCodes.add(Arrays.asList("Евро", "R01239"));
+        currencyCodes.add(Arrays.asList("Фунт стерлингов", "R01035"));
+        currencyCodes.add(Arrays.asList("Российский рубль", "R"));
+    }
+    private static final String url = "http://www.cbr.ru/scripts/XML_dynamic.asp";
 
     //Получить список объектов валюты за последнюю неделю
     public List<DayCurrencyDto> getCurrencyForPeriod(String sourceCurrencyCode) {
